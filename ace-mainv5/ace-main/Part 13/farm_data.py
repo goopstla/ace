bf9b26b44ac878c101cba8ca44897b10fcc09081
goopstla <thegoopgooff@gmail.com>
2024-11-20 13:26:04 +0800

    Add files via upload

---
+++ ace-mainv5/ace-main/Part 13/farm_data.py
@@ -0,0 +1,22 @@
+FARM_DATA = [
+  {
+    #1
+    "cooldown": 50000,  # 50 seconds
+    "money_generated": 200,
+  },
+  {
+    #2
+    "cooldown": 40000,  # 40 seconds
+    "money_generated": 200,
+  },
+  {
+    #3
+    "cooldown": 30000,  # 30 seconds
+    "money_generated": 200,
+  },
+  {
+    #4
+    "cooldown": 26000,  # 26 seconds
+    "money_generated": 200,
+  }
+]
